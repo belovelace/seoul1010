252e3f95baf684171ed043ac7a655f6028d7fbb8
Eun Ji Shin <159417728+belovelace@users.noreply.github.com>
2024-10-10 15:19:51 +0900

    되돌림

--- springWorkspace/app/src/main/resources/static/js/map.js
+++ springWorkspace/app/src/main/resources/static/js/map.js
@@ -27,90 +27,90 @@ function onGeoError(error) {
 }
 
 ////////////////////////카카오 맵 API////////////////////////////////////
-  function showMap(latitude, longitude) {
-            var container = document.getElementById('map');
-            var options = {
-                center: new kakao.maps.LatLng(latitude, longitude),
-                level: 3
-            };
+function showMap(latitude, longitude) {
+    var container = document.getElementById('map');
+    var options = {
+        center: new kakao.maps.LatLng(latitude, longitude),
+        level: 3
+    };
 
-            var map = new kakao.maps.Map(container, options);
+    var map = new kakao.maps.Map(container, options);
 
-            var imageSrc = 'https://cdn-icons-png.flaticon.com/512/2840/2840372.png', // 마커이미지의 주소입니다
-                imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-                imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var imageSrc = 'https://cdn-icons-png.flaticon.com/512/2840/2840372.png', // 마커이미지의 주소입니다
+        imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-            // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
-            // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 -> 추후에는 ajax로 넘기면 될듯
-            var positions = [
-                {
-                    content: '<div class="customoverlay">' +
-                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-                             '    <span class="title">구의야구공원</span>' +
-                             '  </a>' +
-                             '</div>',
-                    latlng: new kakao.maps.LatLng(37.54699, 127.09598)
-                },
-                {
-                    content: '<div class="customoverlay">' +
-                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-                             '    <span class="title">복우물공원</span>' +
-                             '  </a>' +
-                             '</div>',
-                    latlng: new kakao.maps.LatLng(37.455059, 127.128223)
-                },
-                {
-                    content: '<div class="customoverlay">' +
-                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-                             '    <span class="title">효창공원</span>' +
-                             '  </a>' +
-                             '</div>',
-                    latlng: new kakao.maps.LatLng(37.544806, 126.959170)
-                },
-                {
-                    content: '<div class="customoverlay">' +
-                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-                             '    <span class="title">평화의공원</span>' +
-                             '  </a>' +
-                             '</div>',
-                    latlng: new kakao.maps.LatLng(37.562035, 126.894650)
-                }
-            ];
-
-            for (var i = 0; i < positions.length; i++) {
-                // 마커를 생성합니다
-                var marker = new kakao.maps.Marker({
-                    map: map, // 마커를 표시할 지도
-                    position: positions[i].latlng, // 마커의 위치
-                    image: markerImage // 커스텀 마커 이미지
-                });
-
-                // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-                var content = positions[i].content;
-
-                // 커스텀 오버레이를 생성합니다
-                var customOverlay = new kakao.maps.CustomOverlay({
-                    map: map,
-                    position: positions[i].latlng,
-                    content: content,
-                    yAnchor: 1
-                });
-
-                // 기본적으로 커스텀 오버레이는 지도에 표시됩니다. 필요에 따라 표시/숨김을 제어할 수 있습니다.
-                // 예: 커스텀 오버레이를 클릭 시 토글
-                (function(marker, customOverlay) {
-                    kakao.maps.event.addListener(marker, 'click', function() {
-                        if (customOverlay.getMap()) {
-                            customOverlay.setMap(null);
-                        } else {
-                            customOverlay.setMap(map);
-                        }
-                    });
-                })(marker, customOverlay);
-            }
+    // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 -> 추후에는 ajax로 넘기면 될듯
+    var positions = [
+        {
+            content: '<div class="customoverlay">' +
+                '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                '    <span class="title">구의야구공원</span>' +
+                '  </a>' +
+                '</div>',
+            latlng: new kakao.maps.LatLng(37.54699, 127.09598)
+        },
+        {
+            content: '<div class="customoverlay">' +
+                '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                '    <span class="title">복우물공원</span>' +
+                '  </a>' +
+                '</div>',
+            latlng: new kakao.maps.LatLng(37.455059, 127.128223)
+        },
+        {
+            content: '<div class="customoverlay">' +
+                '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                '    <span class="title">효창공원</span>' +
+                '  </a>' +
+                '</div>',
+            latlng: new kakao.maps.LatLng(37.544806, 126.959170)
+        },
+        {
+            content: '<div class="customoverlay">' +
+                '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                '    <span class="title">평화의공원</span>' +
+                '  </a>' +
+                '</div>',
+            latlng: new kakao.maps.LatLng(37.562035, 126.894650)
         }
+    ];
+
+    for (var i = 0; i < positions.length; i++) {
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커의 위치
+            image: markerImage // 커스텀 마커 이미지
+        });
+
+        // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        var content = positions[i].content;
+
+        // 커스텀 오버레이를 생성합니다
+        var customOverlay = new kakao.maps.CustomOverlay({
+            map: map,
+            position: positions[i].latlng,
+            content: content,
+            yAnchor: 1
+        });
+
+        // 기본적으로 커스텀 오버레이는 지도에 표시됩니다. 필요에 따라 표시/숨김을 제어할 수 있습니다.
+        // 예: 커스텀 오버레이를 클릭 시 토글
+        (function(marker, customOverlay) {
+            kakao.maps.event.addListener(marker, 'click', function() {
+                if (customOverlay.getMap()) {
+                    customOverlay.setMap(null);
+                } else {
+                    customOverlay.setMap(map);
+                }
+            });
+        })(marker, customOverlay);
+    }
+}
 
 
 
--- springWorkspace/app/src/main/webapp/WEB-INF/views/util/footer.jsp
+++ springWorkspace/app/src/main/webapp/WEB-INF/views/util/footer.jsp
@@ -1,3 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/util/footer.css">
+    <!-- Google Fonts 추가 (선택 사항) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Grandiflora+One&display=swap" rel="stylesheet">
+</head>
+<body>
+
 <footer>
     <div class="footer-content">
         <h5>잘풀리는 팀</h5>
@@ -5,3 +23,7 @@
         <p>제작자: 박철민, 변경수, 신은지, 홍근재</p>
     </div>
 </footer>
+
+
+</body>
+</html>
